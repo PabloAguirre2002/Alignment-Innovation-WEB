30fbb127254a17fd6275796a9b45c7afc4678485
Michelle Genesoni <genesonimishuu@gmail.com>
2024-04-24 01:16:10 -0300

    modificacion de color de fondo, agregacion de imagen con texto

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+images.forEach(image => {
+    image.addEventListener('mouseover', () => {
+        image.querySelector('.overlay').style.opacity = '1';
+    });
+    image.addEventListener('mouseout', () => {
+        image.querySelector('.overlay').style.opacity = '0';
+    });
+});
